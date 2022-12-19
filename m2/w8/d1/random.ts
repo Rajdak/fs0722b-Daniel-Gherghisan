5b0f339fbfa4969b54981120653b27e003f1790e
Rajdak <114963239+Rajdak@users.noreply.github.com>
2022-12-19 16:25:13 +0100

    w8d1

---
+++ m2/w8/d1/random.ts
@@ -0,0 +1,35 @@
+var num1:number = 0
+var num2:number = 0
+var randomNum:number = 0
+
+var num1= Math.floor(Math.random()*100);
+var num2= Math.floor(Math.random()*100);
+var randomNum=Math.floor(Math.random()*100);
+
+
+console.log("il giocatore numero 1 ha estratto il numero: "+num1);
+console.log("il giocatore numero 2 ha estratto il numero: "+num2);
+console.log("il numero estratto di oggi è: "+randomNum);
+
+if(num1 == randomNum){
+    console.log("il giocatore 1 ha indovinato il numero");
+}
+else if(num2 == randomNum){
+    console.log("il giocatore 2 ha indovinato il numero");
+}
+else{
+    console.log("nessuno dei 2 giocatori hanno indovinato i numeri di oggi")
+    
+    var diff1:number =Math.abs(randomNum - num1)
+    var diff2:number =Math.abs(randomNum - num2)
+    
+   
+    if(diff1 < diff2){
+        console.log("però il giocatore 1 si è avvicinato di più al numero")
+    }
+    else{
+        console.log("però il giocatore 2 si è avvicinato di più al numero")
+    }
+}
+
+
